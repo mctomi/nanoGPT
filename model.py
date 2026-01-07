686e982c8d2d6a35aebc42c8e9792519b68b80c3
Mateus Costamilan Tomiello <mateus.tomiello@gmail.com>
2026-01-07 19:41:00 -0300

    fix

--- model.py
+++ model.py
@@ -98,7 +98,7 @@ class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = Mixer(config)
+        self.attn = DecayMixer(config)
 
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
